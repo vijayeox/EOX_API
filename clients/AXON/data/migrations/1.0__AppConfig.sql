41ac2e44be91a896587f395a2048cbf992cb82ac
soumyan <soumyan@eoxvantage.in>
2021-06-07 17:34:10 +0530

    [REQ] 22077, 22752 - No need of Excel Mapper URL record

--- clients/AXON/data/migrations/1.0__AppConfig.sql
+++ clients/AXON/data/migrations/1.0__AppConfig.sql
@@ -5,16 +5,6 @@ CREATE TABLE IF NOT EXISTS `applicationConfig` (
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
 
-
-INSERT INTO
-    `applicationConfig` (`type`, `value`)
-VALUES
-    (
-        'excelMapperURL',
-        'http://54.161.224.59:5000/api/FileUpload'
-    );
-
-
 INSERT INTO
     `applicationConfig` (`type`, `value`)
 VALUES
