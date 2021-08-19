049b0978730f40a2ea480d889a56244fc99a7374
mkenia <mkenia@hotmail.com>
2021-08-19 11:57:03 -0400

    [BUG] 25857 - MySQL Query JSON returns all strings, instead of numeric

--- api/v1/lib/Oxzion/src/Analytics/Relational/AnalyticsEngineMySQLImpl.php
+++ api/v1/lib/Oxzion/src/Analytics/Relational/AnalyticsEngineMySQLImpl.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace Oxzion\Analytics\Relational;
 
 use Oxzion\Analytics\Relational\AnalyticsEngineRelational;
 use Zend\Db\Adapter\Adapter;
+use Oxzion\Auth\AuthConstants;
+use Oxzion\Auth\AuthContext;
 
 class AnalyticsEngineMySQLImpl extends AnalyticsEngineRelational
 {
@@ -21,5 +24,45 @@ class AnalyticsEngineMySQLImpl extends AnalyticsEngineRelational
         $dbConfig['dsn'] = 'mysql:dbname=' . $config['database'] . ';host=' . $config['host'] . ';charset=utf8;username=' . $config["username"] . ';password=' . $config["password"] . '';
         $this->dbConfig = $dbConfig;
         $this->dbAdapter = new Adapter($dbConfig);
+    }
+
+    public function getData($app_name, $entity_name, $parameters)
+    {
+        $result = parent::getData($app_name, $entity_name, $parameters);
+        if (isset($parameters['view'])) {
+            $entity_name = $parameters['view'];
+        }
+        $result['data'] = $this->changeDataType($result['data'], $entity_name, $parameters);
+        return $result;
+    }
+
+    public function changeDataType($result, $entity_name, $parameters)
+    {
+        $metadata = $this->getFields($entity_name);
+        if (!empty($result) && is_array($result)) {
+            foreach ($result as $rowkey => $row) {
+                if (is_array($row)) {
+                    foreach ($row as $colkey => $column) {
+                        if ($colkey=='count') {
+                            $result[$rowkey][$colkey] = (int) $column;
+                        }
+                        elseif (isset($metadata[$colkey])) {
+                            $type = $metadata[$colkey]['type'];
+                            switch ($type) {
+                                case 'int':
+                                    $result[$rowkey][$colkey] = (int) $column;
+                                    break;
+                                case 'double':
+                                case 'float':
+                                case 'decimal':
+                                    $result[$rowkey][$colkey] = (float) $column;
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return $result;
     }
 }
