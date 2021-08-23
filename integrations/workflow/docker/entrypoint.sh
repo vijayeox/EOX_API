2fc534f04f67cb20c2864d1c2ae96daab0676680
Karan Agarwal <karan@eoxvantage.in>
2021-08-23 22:35:33 +0530

    [REFACTOR] Simplify docker startup | just run ./start.sh on workflow folder.

---
+++ integrations/workflow/docker/entrypoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+if [ ! -e ./IdentityService/dist ]; then
+
+    echo "Building IdentityService..."
+    cd ./IdentityService
+    ./gradlew build
+
+    mkdir -p dist
+    cp ./build/libs/identity_plugin-1.0.jar dist/identity_plugin.jar
+    cd ..
+
+fi
+
+echo "Building ProcessEngine..."
+cd ./ProcessEngine
+./gradlew build
+
+mkdir -p dist
+cp ./build/libs/processengine_plugin-1.0.jar dist/processengine_plugin.jar
+cd ..
