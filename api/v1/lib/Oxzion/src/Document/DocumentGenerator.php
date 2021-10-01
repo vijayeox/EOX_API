5e566091e6436e7626120f122522677b91bc6530
Karan Agarwal <karan@eoxvantage.in>
2021-10-01 22:12:12 +0530

    [REQ] 19288 - ability to flatten fields after inserting data into it and fixed document merge issues

--- api/v1/lib/Oxzion/src/Document/DocumentGenerator.php
+++ api/v1/lib/Oxzion/src/Document/DocumentGenerator.php
@@ -8,9 +8,10 @@ interface DocumentGenerator
     public function generatePdfDocumentFromHtml($htmlContent, $destination, $header = null, $footer = null, array $append = null, array $prepend = null);
     // public function generateDocumentFromFile($filePath,$destination);
 
+    public function mergePdf(array $pdfs, string $destination);
     public function mergeDocuments(array $sourceArray, $destination);
 
-    public function fillPDFForm($template, $data, $destination);
+    public function fillPDFForm($template, $data, $destination, bool $flattenFields = false);
 
     // public function overlayDocument($sourcePdf,$destination,$data);
 }
