8ca5657544176131334d53e88e7e8739a9f257c6
Karan Agarwal <karan@eoxvantage.in>
2021-10-20 12:10:52 +0530

    [REQ] 19288 - address validation on google result.

--- api/v1/lib/Oxzion/src/Utils/AddressUtils.php
+++ api/v1/lib/Oxzion/src/Utils/AddressUtils.php
@@ -185,7 +185,7 @@ class AddressUtils
         if (empty($value)) return false;
         switch ($type) {
             case 'zip':
-                if (preg_match('/\d{4,5}/', $value)) {
+                if (self::getZipDetails($value)) {
                     return (strlen($value) == 4) ? (int) '0'.$value : $value;
                 }
                 break;
@@ -198,7 +198,7 @@ class AddressUtils
         return false;
     }
 
-    public static function getCountryDetails(string $country)
+    public static function searchCountryDetails(string $country)
     {
         $country = self::search('country ' . $country, ['country', 'country_name']);
         if (empty($country)) {
@@ -214,13 +214,29 @@ class AddressUtils
         return current(end($response));
     }
 
-    public static function codeToCountryName(string $code)
-    {
-        $code = strtoupper($code);
-        $countryList = self::getCountryList();
-        return isset($countryList[$code]) ? $countryList[$code] : false;
+    // might have blanks for specific zips in less popurlar countries. eg. 560061, IN
+    public static function getZipDetails($zip, string $countryISO2 = 'US') {
+        $response = [];
+        if (!is_numeric($zip)) return $response;
+        if (strtoupper($countryISO2) == 'US') {
+            $client = new RestClient('https://ziptasticapi.com/');
+            $response = json_decode($client->get((string) $zip), true);
+        }
+        if (empty($response) || isset($response['error'])) {
+            $client = new RestClient("https://api.zippopotam.us/$countryISO2/");
+            $response = json_decode($client->get($zip), true);
+            if (!empty($response)) {
+                $response = [
+                    "country" => strtoupper($response['country abbreviation']),
+                    "state" => strtoupper(current($response['places'])['state abbreviation']),
+                    "city" => strtoupper(current($response['places'])['place name'])
+                ];
+            }
+        }
+        return $response;
     }
 
+    // currently works only for us states
 	public static function getStateList(string $code = null, bool $checkName = false)
 	{
         $states = json_decode('{"AE":"Armed Forces Europe","AK":"Alaska","AL":"Alabama","AP":"Armed Forces Pacific","AR":"Arkansas","AZ":"Arizona","CA":"California","CO":"Colorado","CT":"Connecticut","DC":"District of Columbia","DE":"Delaware","FL":"Florida","FM":"Micronesia","GA":"Georgia","GU":"Guam","HI":"Hawaii","IA":"Iowa","ID":"Idaho","IL":"Illinois","IN":"Indiana","International":"International","KS":"Kansas","KY":"Kentucky","LA":"Louisiana","MA":"Massachusetts","MB":"Manitoba","MD":"Maryland","ME":"Maine","MH":"Marshall Islands","MI":"Michigan","MN":"Minnesota","MO":"Missouri","MP":"Northern Marianas","MS":"Mississippi","MT":"Montana","NC":"North Carolina","ND":"North Dakota","NE":"Nebraska","NH":"New Hampshire","NJ":"New Jersey","NM":"New Mexico","NV":"Nevada","NY":"New York","OH":"Ohio","OK":"Oklahoma","ON":"Ontario","OR":"Oregon","PA":"Pennsylvania","PR":"Puerto Rico","PW":"Palau","RI":"Rhode Island","SC":"South Carolina","SD":"South Dakota","TN":"Tennessee","TX":"Texas","UT":"Utah","VA":"Virginia","VI":"Virgin Islands","VT":"Vermont","WA":"Washington","WI":"Wisconsin","WV":"West Virginia","WY":"Wyoming"}', true);
@@ -230,13 +246,46 @@ class AddressUtils
         return empty($code) ? $states : (isset($states[strtoupper($code)]) ? $states[strtoupper($code)] : null);
 	}
 
-    public static function getCountryList(string $code = null, bool $checkName = false)
+    public static function getCountryDetails(array $listConfig = [], string $search = null, string $checkValue = 'ISO2')
     {
-        $countryList = json_decode('{"AF":"Afghanistan","AX":"Aland Islands","AL":"Albania","DZ":"Algeria","AS":"American Samoa","AD":"Andorra","AO":"Angola","AI":"Anguilla","AQ":"Antarctica","AG":"Antigua and Barbuda","AR":"Argentina","AM":"Armenia","AW":"Aruba","AU":"Australia","AT":"Austria","AZ":"Azerbaijan","BS":"Bahamas the","BH":"Bahrain","BD":"Bangladesh","BB":"Barbados","BY":"Belarus","BE":"Belgium","BZ":"Belize","BJ":"Benin","BM":"Bermuda","BT":"Bhutan","BO":"Bolivia","BA":"Bosnia and Herzegovina","BW":"Botswana","BV":"Bouvet Island (Bouvetoya)","BR":"Brazil","IO":"British Indian Ocean Territory (Chagos Archipelago)","VG":"British Virgin Islands","BN":"Brunei Darussalam","BG":"Bulgaria","BF":"Burkina Faso","BI":"Burundi","KH":"Cambodia","CM":"Cameroon","CA":"Canada","CV":"Cape Verde","KY":"Cayman Islands","CF":"Central African Republic","TD":"Chad","CL":"Chile","CN":"China","CX":"Christmas Island","CC":"Cocos (Keeling) Islands","CO":"Colombia","KM":"Comoros the","CD":"Congo","CG":"Congo the","CK":"Cook Islands","CR":"Costa Rica","CI":"Cote d\'Ivoire","HR":"Croatia","CU":"Cuba","CY":"Cyprus","CZ":"Czech Republic","DK":"Denmark","DJ":"Djibouti","DM":"Dominica","DO":"Dominican Republic","EC":"Ecuador","EG":"Egypt","SV":"El Salvador","GQ":"Equatorial Guinea","ER":"Eritrea","EE":"Estonia","ET":"Ethiopia","FO":"Faroe Islands","FK":"Falkland Islands (Malvinas)","FJ":"Fiji the Fiji Islands","FI":"Finland","FR":"France, French Republic","GF":"French Guiana","PF":"French Polynesia","TF":"French Southern Territories","GA":"Gabon","GM":"Gambia the","GE":"Georgia","DE":"Germany","GH":"Ghana","GI":"Gibraltar","GR":"Greece","GL":"Greenland","GD":"Grenada","GP":"Guadeloupe","GU":"Guam","GT":"Guatemala","GG":"Guernsey","GN":"Guinea","GW":"Guinea-Bissau","GY":"Guyana","HT":"Haiti","HM":"Heard Island and McDonald Islands","VA":"Holy See (Vatican City State)","HN":"Honduras","HK":"Hong Kong","HU":"Hungary","IS":"Iceland","IN":"India","ID":"Indonesia","IR":"Iran","IQ":"Iraq","IE":"Ireland","IM":"Isle of Man","IL":"Israel","IT":"Italy","JM":"Jamaica","JP":"Japan","JE":"Jersey","JO":"Jordan","KZ":"Kazakhstan","KE":"Kenya","KI":"Kiribati","KP":"Korea","KR":"Korea","KW":"Kuwait","KG":"Kyrgyz Republic","LA":"Lao","LV":"Latvia","LB":"Lebanon","LS":"Lesotho","LR":"Liberia","LY":"Libyan Arab Jamahiriya","LI":"Liechtenstein","LT":"Lithuania","LU":"Luxembourg","MO":"Macao","MK":"Macedonia","MG":"Madagascar","MW":"Malawi","MY":"Malaysia","MV":"Maldives","ML":"Mali","MT":"Malta","MH":"Marshall Islands","MQ":"Martinique","MR":"Mauritania","MU":"Mauritius","YT":"Mayotte","MX":"Mexico","FM":"Micronesia","MD":"Moldova","MC":"Monaco","MN":"Mongolia","ME":"Montenegro","MS":"Montserrat","MA":"Morocco","MZ":"Mozambique","MM":"Myanmar","NA":"Namibia","NR":"Nauru","NP":"Nepal","AN":"Netherlands Antilles","NL":"Netherlands the","NC":"New Caledonia","NZ":"New Zealand","NI":"Nicaragua","NE":"Niger","NG":"Nigeria","NU":"Niue","NF":"Norfolk Island","MP":"Northern Mariana Islands","NO":"Norway","OM":"Oman","PK":"Pakistan","PW":"Palau","PS":"Palestinian Territory","PA":"Panama","PG":"Papua New Guinea","PY":"Paraguay","PE":"Peru","PH":"Philippines","PN":"Pitcairn Islands","PL":"Poland","PT":"Portugal, Portuguese Republic","PR":"Puerto Rico","QA":"Qatar","RE":"Reunion","RO":"Romania","RU":"Russian Federation","RW":"Rwanda","BL":"Saint Barthelemy","SH":"Saint Helena","KN":"Saint Kitts and Nevis","LC":"Saint Lucia","MF":"Saint Martin","PM":"Saint Pierre and Miquelon","VC":"Saint Vincent and the Grenadines","WS":"Samoa","SM":"San Marino","ST":"Sao Tome and Principe","SA":"Saudi Arabia","SN":"Senegal","RS":"Serbia","SC":"Seychelles","SL":"Sierra Leone","SG":"Singapore","SK":"Slovakia (Slovak Republic)","SI":"Slovenia","SB":"Solomon Islands","SO":"Somalia, Somali Republic","ZA":"South Africa","GS":"South Georgia and the South Sandwich Islands","ES":"Spain","LK":"Sri Lanka","SD":"Sudan","SR":"Suriname","SJ":"Svalbard & Jan Mayen Islands","SZ":"Swaziland","SE":"Sweden","CH":"Switzerland, Swiss Confederation","SY":"Syrian Arab Republic","TW":"Taiwan","TJ":"Tajikistan","TZ":"Tanzania","TH":"Thailand","TL":"Timor-Leste","TG":"Togo","TK":"Tokelau","TO":"Tonga","TT":"Trinidad and Tobago","TN":"Tunisia","TR":"Turkey","TM":"Turkmenistan","TC":"Turks and Caicos Islands","TV":"Tuvalu","UG":"Uganda","UA":"Ukraine","AE":"United Arab Emirates","GB":"United Kingdom","US":"United States of America","UM":"United States Minor Outlying Islands","VI":"United States Virgin Islands","UY":"Uruguay, Eastern Republic of","UZ":"Uzbekistan","VU":"Vanuatu","VE":"Venezuela","VN":"Vietnam","WF":"Wallis and Futuna","EH":"Western Sahara","YE":"Yemen","ZM":"Zambia","ZW":"Zimbabwe","Oth":"Others"}', true);
-        if ($checkName) {
-            $countryList = array_flip(array_map('strtoupper', $countryList));
+        if (empty($listConfig)) $listConfig = ['key' => 'ISO2', 'value' => null];
+        $detailList = [
+            // 'ISO2',
+            'ISO3' => json_decode(file_get_contents('http://country.io/iso3.json')),
+            'Country' => json_decode(file_get_contents('http://country.io/names.json')),
+            'Phone' => json_decode(file_get_contents('http://country.io/phone.json')),
+            'Currency' => json_decode(file_get_contents('http://country.io/currency.json')),
+            'Continent' => json_decode(file_get_contents('http://country.io/continent.json'))
+        ];
+        $details = [];
+        foreach ($detailList as $index => $list) {
+            foreach ($list as $code => $value) {
+                if (empty($details[$code]['ISO2'])) $details[$code]['ISO2'] = $code;
+                $details[$code][$index] = $value;
+            }
         }
-        return empty($code) ? $countryList : (isset($countryList[strtoupper($code)]) ? $countryList[strtoupper($code)] : null);
+        // echo "<pre>";print_r(json_encode($details));exit;
+        if (!empty($search)) {
+            $search = strtoupper(trim($search));
+            $detailsTemp = array_change_key_case(array_column($details, null, $checkValue), CASE_UPPER);
+            switch ($checkValue) {
+                case 'Country':
+                    if (isset($detailsTemp[$search])) {
+                        return $detailsTemp[$search];
+                    }
+                    foreach ($detailsTemp as $key => $detail) {
+                        if (strpos($key, $search) !== false) {
+                            return $detail;
+                        }
+                    }
+                    return null;
+                    break;
+                default:
+                    return isset($detailsTemp[$search]) ? $detailsTemp[$search] : null;
+                    break;
+            }
+        }
+        return array_column($details, $listConfig['value'], $listConfig['key']);
     }
 
 }
