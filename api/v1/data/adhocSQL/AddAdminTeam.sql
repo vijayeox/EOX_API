de3b4979a7a6e3635fa48e25b7e63ce01ef59646
sagarsp <sagar@eoxvantage.com>
2021-11-11 13:08:55 +0530

    [REQ] Added migration for Admin Team Team Creation

---
+++ api/v1/data/adhocSQL/AddAdminTeam.sql
@@ -0,0 +1,13 @@
+INSERT INTO 
+    `ox_team` (`uuid`,`name`,`account_id`,`manager_id`,`description`,`status`,`date_created`,`created_id`)
+VALUES 
+    (
+        '640436b6-112f-4dd5-9cb3-60b7ce775c90',
+        'Admin Team',
+        3,
+        6,
+        'Admin Team',
+        'Active',
+        '2021-10-29 09:38:42',
+        6
+    );
