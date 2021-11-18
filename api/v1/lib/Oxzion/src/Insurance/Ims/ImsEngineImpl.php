27d52c00c9e564d59d2a24724741aa460ffae914
Karan Agarwal <karan@eoxvantage.com>
2021-11-19 03:03:44 +0530

    [REQ] 19288 - updated IMS credentials to be independent of the application.

--- api/v1/lib/Oxzion/src/Insurance/Ims/ImsEngineImpl.php
+++ api/v1/lib/Oxzion/src/Insurance/Ims/ImsEngineImpl.php
@@ -28,6 +28,10 @@ class ImsEngineImpl implements InsuranceEngine
     }
     public function setConfig($data)
     {
+        if (empty($data['wsdlUrl']) || empty($data['userName']) || empty($data['tripleDESEncryptedPassword'])) {
+            throw new \Exception("Insurance Management Systems auth not provided.", 404);
+        }
+        $this->config['ims'] = $data;
         $this->setSoapClient($data['handle']);
     }
     private function setSoapClient($handle)
