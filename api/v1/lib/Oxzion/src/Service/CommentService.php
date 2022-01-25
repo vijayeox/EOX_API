60122d43beab75c37bf8a59edc96f30c5e736063
Rakshith <rakshith@vantageagora.com>
2022-01-25 16:52:47 +0530

    Revert "[REFACTOR] Saving Attachments to comments"
    This reverts commit 06d0df9a4d840f8f1cb92795af82cf699a817215.

--- api/v1/lib/Oxzion/src/Service/CommentService.php
+++ api/v1/lib/Oxzion/src/Service/CommentService.php
@@ -53,9 +53,7 @@ class CommentService extends AbstractService
         $data['modified_by'] = AuthContext::get(AuthConstants::USER_ID);
         $data['date_created'] = isset($data['date_created']) ? $data['date_created'] : date('Y-m-d H:i:s');
         $data['date_modified'] = date('Y-m-d H:i:s');
-        if (isset($data['attachments'])) {            
-            $data['attachments'] = json_encode($data['attachments']);
-        }
+        $data['attachments'] = json_encode($data['attachments']);
         if (isset($data['parent'])) {
             $ret = $this->getParentId($data, $fileId);
             if (!$ret) {
