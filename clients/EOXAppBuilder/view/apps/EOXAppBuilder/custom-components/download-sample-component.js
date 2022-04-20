29cdb552f5ff4b61f56d3792bbb8fc84625fd818
mukuldutt47 <mukuld@eoxvantage.in>
2022-04-20 13:11:10 +0530

    [REQ] #34322 sample custom component+

---
+++ clients/EOXAppBuilder/view/apps/EOXAppBuilder/custom-components/download-sample-component.js
@@ -0,0 +1,40 @@
+import { React } from "oxziongui";
+export default function SampleComponentDownload(props) {
+  return (
+    <SampleDownloadComponent
+      core={props.core}
+      appId={props.appId}
+      parentPageData={props.parentPageData}
+      data={props.data}
+    />
+  );
+}
+class SampleDownloadComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <a
+        style={{
+          background: "#007bff",
+          color: "#FFF",
+          padding: " 0.6rem",
+          borderRadius: "6px",
+          fontSize: "12px",
+          position: "absolute",
+          top: ".5rem",
+          backgroundColor: "#e4e7eb",
+          color: "#000",
+          right: "3.75rem",
+        }}
+        download={"sameple-component.js"}
+        href={"/apps/EOXAppBuilder/appstudio/samplecomponent"}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Click here to download sample component
+      </a>
+    );
+  }
+}
--- clients/EOXAppBuilder/view/apps/EOXAppBuilder/server.js
+++ clients/EOXAppBuilder/view/apps/EOXAppBuilder/server.js
@@ -1,27 +1,34 @@
 // Methods OS.js server requires
-
+const path = require('path')
 require('@babel/register')({
     ignore: [ /(node_modules)/ ],
     presets: ['@babel/env','@babel/react']
 });
-module.exports = (core, proc) => ({
+module.exports = (core, proc) => {
+  const { routeAuthenticated } = core.make('osjs/express');
+  return {
 
-  // When server initializes
-  init: async () => {
-    // HTTP Route example (see index.js)
-    core.app.post(proc.resource('/test'), (req, res) => {
-      res.json({hello: 'World'});
-    });
-  },
+    // When server initializes
+    init: async () => {
+      // HTTP Route example (see index.js)
+      core.app.post(proc.resource('/test'), (req, res) => {
+        res.json({hello: 'World'});
+      });
 
-  // When server starts
-  start: () => {},
-
-  // When server goes down
-  destroy: () => {},
-
-  // When using an internally bound websocket, messages comes here
-  onmessage: (ws, respond, args) => {
-    respond('Pong');
+      routeAuthenticated('GET', proc.resource('/appstudio/samplecomponent'), async (req, res) => {
+        res.sendFile(path.join(__dirname + '/public/img/samplecustom-component.js'))
+      });
+    },
+  
+    // When server starts
+    start: () => {},
+  
+    // When server goes down
+    destroy: () => {},
+  
+    // When using an internally bound websocket, messages comes here
+    onmessage: (ws, respond, args) => {
+      respond('Pong');
+    }
   }
-});
+}
