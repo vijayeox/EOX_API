725603ce995e6c253479c8712bc2f452401e5898
mukuldutt47 <mukuld@eoxvantage.in>
2022-06-21 18:12:30 +0530

    [REFACTOR] #36561 : fixed close popup text

---
+++ clients/EOXAppBuilder/view/apps/EOXAppBuilder/custom-components/app-studio/app-studio.js
@@ -0,0 +1,13 @@
+import { React } from "oxziongui";
+import './app-studio.scss'
+export default function AppStudioComp() {
+  return <AppStudio />;
+}
+class AppStudio extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return null;
+  }
+}
